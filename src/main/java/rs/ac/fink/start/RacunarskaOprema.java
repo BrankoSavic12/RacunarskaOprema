6778061c5bb6645aa20c885008256b842fa7913e
BrankoSavic12 <brankosavic2001@gmail.com>
2024-12-16 21:47:57 +0100

    Dodata Postman kolekcija

--- src/main/java/rs/ac/fink/start/RacunarskaOprema.java
+++ src/main/java/rs/ac/fink/start/RacunarskaOprema.java
@@ -22,23 +22,33 @@ public class RacunarskaOprema {
             UserService userService = UserService.getInstance();
             System.out.println("\n=== Operacije sa korisnicima ===");
 
-            User newUser = new User(0, "Jovan Jovanovic44", "jjovan", "jovan.jovanovic@example.com", "1988-03-12", 1000, 5000);
+            User newUser = new User(0, "Jovan Jovanovic44", "jjovan", "jovan.jovanovic@example.com", "1988-03-12", 10000, 5000);
             int userId = userService.addUser(newUser);
             System.out.println("Dodat korisnik sa ID: " + userId);
 
             User retrievedUser = userService.getUserById(userId);
             System.out.println("Preuzet korisnik: " + retrievedUser);
 
+            // Brisanje korisnika
+            System.out.println("Brisanje korisnika sa ID: " + userId);
+            userService.deleteUser(userId);
+            System.out.println("Korisnik obrisan.");
+
             // Operacije za proizvode
             ProductService productService = ProductService.getInstance();
             System.out.println("\n=== Operacije sa proizvodima ===");
 
-            Product newProduct = new Product(0, "Grafička karta", 7500, "Computer Hardware", 5);
+            Product newProduct = new Product(0, "Grafička karta", 1500, "Computer Hardware", 5);
             int productId = productService.addProduct(newProduct);
             System.out.println("Dodat proizvod sa ID: " + productId);
 
             Product retrievedProduct = productService.getProductById(productId);
             System.out.println("Preuzet proizvod: " + retrievedProduct);
+
+            // Brisanje proizvoda
+            System.out.println("Brisanje proizvoda sa ID: " + productId);
+            productService.deleteProduct(productId);
+            System.out.println("Proizvod obrisan.");
 
             // Operacije za kupovine
             PurchaseService purchaseService = PurchaseService.getInstance();
@@ -48,11 +58,11 @@ public class RacunarskaOprema {
             int purchaseId = purchaseService.addPurchase(newPurchase);
             System.out.println("Dodata kupovina sa ID: " + purchaseId);
 
-            
         } catch (RacunarskaOpremaException e) {
-            System.err.println("Greska u aplikaciji: " + e.getMessage());
+            System.err.println("Greška u aplikaciji: " + e.getMessage());
         } catch (Exception e) {
-            System.err.println("Neocekivana greška: " + e.getMessage());
+            System.err.println("Neočekivana greška: " + e.getMessage());
         }
     }
 }
+
